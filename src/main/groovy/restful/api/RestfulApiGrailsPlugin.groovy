49d7d166d8d3aee3668b29babb7b1c88f51e6592
Soven Rout <sovenr@ellucian.com>
2019-01-28 08:13:00 +0530

    HRU-8454 : Changes related to Restful-API plugin migration

--- src/main/groovy/restful/api/RestfulApiGrailsPlugin.groovy
+++ src/main/groovy/restful/api/RestfulApiGrailsPlugin.groovy
@@ -1,70 +1,66 @@
-package resful.api
+/* ****************************************************************************
+ * Copyright 2013-2018 Ellucian Company L.P. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 
-import grails.plugins.*
+import grails.converters.JSON
 
-class ResfulApiGrailsPlugin extends Plugin {
+import net.hedtech.restfulapi.*
+import net.hedtech.restfulapi.marshallers.json.*
 
-    // the version or versions of Grails the plugin is designed for
+import org.grails.web.converters.configuration.ConvertersConfigurationHolder as CCH
+import org.grails.web.converters.configuration.DefaultConverterConfiguration as DCC
+
+class RestfulApiGrailsPlugin {
+
+    def version = "1.7.0"
     def grailsVersion = "3.3.2 > *"
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+            "grails-app/views/**",
+            "web-app/**"
     ]
 
-    // TODO Fill in these fields
-    def title = "Resful Api" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "RESTful API Plugin"
+    def author = "Charles Hardt, Shane Riddell"
+    def authorEmail = "shane_riddell@icloud.com"
+    def developers = [
+            [name: "Charlie Hardt", email: "chasdev@me.com"]
+    ]
     def description = '''\
-Brief summary/description of the plugin.
-'''
-    def profiles = ['web']
+        |The resful-api plugin facilitates exposing a non-trivial,
+        | versioned RESTful API. The plugin provides a DSL that may
+        | be used to declaratively specify how resources should be
+        | marshalled. Please see the README.md for details.
+        |'''.stripMargin()
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/resful-api"
+    def documentation = "https://github.com/restfulapi/restful-api/blob/master/README.md"
 
-    // Extra (optional) plugin metadata
+    def scm = [url: "https://github.com/restfulapi/restful-api.git"]
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/restfulapi/restful-api/issues" ]
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def license = "APACHE"
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+// ----------------------------------------------------------------------------
 
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    Closure doWithSpring() { {->
-            // TODO Implement runtime spring config (optional)
-        }
-    }
-
-    void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
+    def doWithApplicationContext = { applicationContext ->
+        // Initialize the Restful API controller (so it will register JSON and XML marshallers)
+        //
+        def artefact = application.getArtefactByLogicalPropertyName("Controller", "restfulApi")
+        def restfulApiController = applicationContext.getBean(artefact.clazz.name)
+        restfulApiController.init()
     }
 }
+
