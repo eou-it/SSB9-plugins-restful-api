74c06a16b919cc49b4706a9ab746658ccc123665
Shrikanth sarma <shrikanth.sarma@ellucian.com>
2019-04-11 09:34:38 +0530

    Url Mapping changes

--- grails-app/controllers/resful/api/UrlMappings.groovy
+++ grails-app/controllers/resful/api/UrlMappings.groovy
@@ -3,14 +3,27 @@ package resful.api
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+
+        "/api/$pluralizedResourceName/$id"(controller: 'restfulApi') {
+            action = [GET: "show", PUT: "update", DELETE: "delete"]
+            parseRequest = false
+        }
+
+        "/api/$pluralizedResourceName"(controller: 'restfulApi') {
+            action = [GET: "list", POST: "create"]
+            parseRequest = false
+        }
+
+        "/$controller/$action?/$id?" {
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        "/"(view: "/index")
+        "500"(view: '/error')
+
     }
+
 }
+
