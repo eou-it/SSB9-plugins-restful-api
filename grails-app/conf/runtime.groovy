75b2aafc290567be5c9cce9552cbac10ec062455
Lokesh <lokesh.nagappa@ellucian.com>
2020-03-30 12:55:56 +0530

    moved the restful api configuration from application.groovy to runtime.groovy

---
+++ grails-app/conf/runtime.groovy
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ Copyright 2019 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+
+restfulApiConfig = {
+    // Resources for web_app_extensibility plugin
+    resource 'extensions' config {
+        //  def a = new BasicDomainClassMarshaller()
+        // println a
+    }
+}
