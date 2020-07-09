7abf6c562bb213bc028dc54a068f43ec993e7ec4
swaroopsg <swaroop.sg@ellucian.com>
2020-07-09 11:29:27 +0530

    added fetching version for delete in restful controller

--- grails-app/controllers/net/hedtech/restfulapi/RestfulApiController.groovy
+++ grails-app/controllers/net/hedtech/restfulapi/RestfulApiController.groovy
@@ -555,7 +555,10 @@ class RestfulApiController {
             }
             checkId(content, resourceConfig)
             if(resourceConfig?.serviceName == "specDrivenAPIDataModelFacadeService"){
+
+                def responseRepresentation = getResponseRepresentation(resourceConfig)
                 params?.serviceName = "specDrivenAPIDataModelFacadeService"
+                params?.majorVersion = resourceConfig?.representations[responseRepresentation?.mediaType]?.apiVersion?.majorVersion
             }
             getServiceAdapter(resourceConfig).delete( getService(resourceConfig), content, params )
             response.setStatus( 200 )
